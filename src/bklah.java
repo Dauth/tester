451a616497757461d495692c66c7ba7cd710e65e
gary <garywjd91@gmail.com>
2015-03-19 12:38:24 +0800

    roughly completed. need to test relaxed

--- src/bklah.java
+++ src/bklah.java
@@ -13,22 +13,25 @@ import com.joestelmach.natty.Parser;
 public class bklah {
 	public static void main(String [] args){
 		Parser parser = new Parser();
-		List<DateGroup> groups = parser.parse("do by 4 days");
+		List<DateGroup> groups = parser.parse("do by 2015/15/12");
+		//List<DateGroup> groups = parser.parse("do by 4 days");
 		for(DateGroup group: groups){
 			  List dates = group.getDates();
-			  int line = group.getLine();
-			  int column = group.getPosition();
-			  String matchingValue = group.getText();
-			  String syntaxTree = group.getSyntaxTree().toStringTree();
-			  String[] temp=dates.toString().split(" ");
-			  System.out.println(dates.toString());
-			  String a=temp[2]+"/"+temp[1]+"/"+temp[5].replace("]", "");
-			System.out.println(temp[2]+"/"+temp[1]+"/"+temp[5].replace("]", ""));
+//			  int line = group.getLine();
+//			  int column = group.getPosition();
+//			  String matchingValue = group.getText();
+//			  String syntaxTree = group.getSyntaxTree().toStringTree();
+//			  String[] temp=dates.toString().split(" ");
+//			  System.out.println(dates.toString());
+//			  String a=temp[2]+"/"+temp[1]+"/"+temp[5].replace("]", "");
+//			System.out.println(temp[2]+"/"+temp[1]+"/"+temp[5].replace("]", ""));
+			SimpleDateFormat actual = new SimpleDateFormat("[EEE MMM dd HH:mm:ss z yyyy]");
 			SimpleDateFormat sdf=new SimpleDateFormat("dd/MMM/yyyy");
 			SimpleDateFormat sdf2=new SimpleDateFormat("dd/MM/yyyy");
 			String newDate;
+			System.out.println("Actual format "+dates.toString());
 			try {
-				newDate = sdf2.format(sdf.parse(a));
+				newDate = sdf2.format(actual.parse(dates.toString()));
 				System.out.println("done "+newDate);
 			} catch (ParseException e) {
 				// TODO Auto-generated catch block
